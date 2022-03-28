5c215faeef475e3dd42b4ea5058450d8077f7647
aaronr8684 <57102156+aaronr8684@users.noreply.github.com>
2022-03-28 10:14:06 -0400

    Create env.py

---
+++ src/env.py
@@ -0,0 +1,6 @@
+## CONFIGURE THIS VIA THE GUIDE - https://developer.tuya.com/en/docs/iot/quick-start1?id=K95ztz9u9t89n
+# ACCESS_ID = # your_access_id
+# ACCESS_KEY = # your_access_key
+# USERNAME = # your_username
+# PASSWORD = # your_password
+# ENDPOINT = "https://openapi.tuyacn.com"
