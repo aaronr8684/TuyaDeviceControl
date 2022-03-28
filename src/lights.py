3293b1d1955b8435752113245ec5c64bd194a70e
aaronr8684 <57102156+aaronr8684@users.noreply.github.com>
2022-03-28 12:54:17 -0400

    Add support for multi-device commands

--- src/lights.py
+++ src/lights.py
@@ -9,26 +9,34 @@ from tuya_iot import (
 
 TUYA_LOGGER.setLevel(logging.DEBUG)
 
-DEVICE_ID = sys.argv[1]
+DEVICE_IDs = sys.argv[1]
 ACTION = sys.argv[2]
 VALUE = sys.argv[3]
+
+device_list = DEVICE_IDs.split(',')
 
 # Init
 openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY, AuthType.CUSTOM)
 
 openapi.connect(USERNAME, PASSWORD)
 
+# Do Commands
+for DEVICE_ID in device_list:
+    if ACTION == 'switch_led':
+        commands = {'commands': [{'code': ACTION, 'value': VALUE == 'True'}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
 
-if ACTION == 'switch_led':
-    commands = {'commands': [{'code': ACTION, 'value': VALUE == 'True'}]}
-    openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
-
-if ACTION == 'colour_data':
-    commands = {'commands': [{'code': 'work_mode', 'value': 'colour'}]}
-    openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
-    BRIGHT = sys.argv[4]    
-    commands = {'commands': [{'code': ACTION, 'value': "{\"h\":"+VALUE+",\"s\":255.0,\"v\":"+BRIGHT+"}"}]}
-    openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
-    commands = {'commands': [{'code': 'switch_led', 'value': True}]}
-    openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
-    
+    if ACTION == 'colour_data':
+        commands = {'commands': [{'code': 'work_mode', 'value': 'colour'}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
+        BRIGHT = sys.argv[4]    
+        commands = {'commands': [{'code': ACTION, 'value': "{\"h\":"+VALUE+",\"s\":255.0,\"v\":"+BRIGHT+"}"}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
+        commands = {'commands': [{'code': 'switch_led', 'value': True}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
+        
+    if ACTION == 'bright_value':
+        commands = {'commands': [{'code': ACTION, 'value': int(VALUE)}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
+        commands = {'commands': [{'code': 'switch_led', 'value': True}]}
+        openapi.post('/v1.0/iot-03/devices/{}/commands'.format(DEVICE_ID), commands)
